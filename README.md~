6acdcaf1559492a902b209d6ba5136c84ebd3556
bhanuc <bhanuc@iitk.ac.in>
2015-09-02 19:38:14 +0530

    Reiterated first build

---
+++ README.md~
@@ -0,0 +1,41 @@
+##Course Website Assistant for College Professors
+
+A cross Platform Desktop Client which lets Busy Professors create a course Website and regularly update it.
+
+### Already implemented features
+	
+	Download zip
+
+	Course Templates
+
+	Material Theme
+
+### Pending features
+	
+	Add Media Uploads
+	
+	Push to Github Pages
+
+	SFTP Push
+
+	Implement Custom pages
+
+	An Android Client
+
+##Project in Alpha - Target Testing Phase (2016 -)
+
+##To run -
+ Download the release from [here](https://github.com/navya/Kalam/releases). Installer coming soon. Only Linux binary available. Open a issue if you want to maintain windows and mac release binaries.
+
+##Dev Build Process
+	Run the following command. 
+	chmod +x build-linux-x64.sh
+	./build-linux-x64.sh
+
+
+	More Powerful build process coming
+
+##Contributions : 
+Please test and open a issue. Before Working on anything major, please check if its already being worked upon.
+
+##Project By - Bhanu Pratap Chaudhary
---
+++ source/js/db_func.js
@@ -0,0 +1,47 @@
+var db = new PouchDB('courses');
+var remoteCouch = false;
+
+var add_course = function(course) {
+	db.put(course, function callback(err, result) {
+		if (!err) {
+			alertify.success('The course has been created')
+		}
+	});
+
+}
+
+function showcourse() {
+	db.allDocs({
+		include_docs: true,
+		descending: true
+	}, function(err, doc) {
+		var rows = doc.rows;
+		if (rows.length) {
+			var str = ''
+			for (var i = rows.length - 1; i >= 0; i--) {
+				str += getstr(rows[i].doc)
+			}
+			document.getElementById('main').innerHTML = str;
+		} else {
+			alertify.success('No courses found.')
+		}
+
+	});
+}
+
+function updatecoursebyid(id, course) {
+
+	db.get(id).then(function(doc) {
+		course._rev = doc._rev;
+		course._id = id;
+		return db.put(course);
+	}).then(function(response) {
+		alertify.success('course updated.')
+	}).catch(function(err) {
+		console.log(JSON.stringify(err));
+	});
+}
+
+var getstr = function(course) {
+	return '<div class="demo-card-event mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-grid mdl-grid--no-spacing"><div class="mdl-card__title mdl-card--expand"><h2 class="mdl-card__title-text">' + (course.name || course.title) + '</h2> </div><div class="mdl-card__supporting-text mdl-color-text--white-600">            Upcoming quiz: Tomorrow<br> Upcoming Assignment: 14th          </div>          <div class="mdl-card__actions mdl-card--border" style="background-color: white;color: #46B6AC;"><a href="single.html#course/' + course._id + '"class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" style="color: #46B6AC;">Open</a><div class="mdl-layout-spacer"></div><i class="material-icons">open_in_browser</i></div></div>'
+}
