c17fa3753437447e679e964b420cd98712b40a30
bhanuc <bhanuc@iitk.ac.in>
2015-09-26 00:16:44 +0530

    transferring the user files to config folder

--- app/js/all.js
+++ app/js/all.js
@@ -1,14 +1,22 @@
-   var appData = app.getPath("appData");
+var remote = require('remote')
+var app = remote.require('app')
+var appData = app.getPath("appData");
+var path = require('path');
+var fs = require('fs');
 
 
-   function toggleabout() {
-   	el = document.getElementById("about");
-   	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-   }
-   //Should be called everytime the app opens
-   function bootstrapFolder() {
-   	var dir = path.join(appData, 'courses')
-   	if (!fs.existsSync(dir)) {
-   		fs.mkdirSync(dir);
-   	}
-   }
+function toggleabout() {
+	el = document.getElementById("about");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+//Should be called everytime the app opens
+function bootstrapFolder() {
+	var dir = path.join(appData, 'kalam')
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir);
+	}
+	var dir2 = path.join(dir, 'courses')
+	if (!fs.existsSync(dir2)) {
+		fs.mkdirSync(dir2);
+	}
+}
